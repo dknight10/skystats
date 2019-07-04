0f1332536f8ec0a6eaa3568dce07baada6b0fe76
dknight <chaknight@deloitte.com>
2019-07-04 10:20:32 -0400

    Add shot model

--- api/v1/shots/models.py
+++ api/v1/shots/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+# TODO add validators for number fields
+class Shot(models.Model):
+    HAND_CHOICES = [("L", "left"), ("R", "right")]
+    shot_num = models.IntegerField()
+    hand = models.CharField(max_length=1, choices=HAND_CHOICES)
+    ball_speed = models.IntegerField()
+    launch_angle = models.DecimalField(max_digits=3, decimal_places=1)
+    back_spin = models.IntegerField()
+    side_spin = models.IntegerField()
+    side_angle = models.DecimalField(max_digits=3, decimal_places=1)
+    offline_distance = models.IntegerField()
+    carry = models.IntegerField()
+    roll = models.IntegerField()
+    total = models.IntegerField()
+    hang_time = models.DecimalField(max_digits=3, decimal_places=1)
+    descent_angle = models.DecimalField(max_digits=3, decimal_places=1)
+    peak_height = models.IntegerField()
+    club_speed = models.IntegerField()
+    pti = models.DecimalField(max_digits=3, decimal_places=2)
+    club = models.CharField(max_length=20)  # TODO add choices
+    name = models.CharField(max_length=100)
+    timestamp = models.DateField()
+    session_type = models.CharField(max_length=20)  # TODO add choices
