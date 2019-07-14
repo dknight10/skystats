dd3fe60dbd854a01f06e950a85d680f7ce7b76bf
dknight <chaknight@deloitte.com>
2019-07-14 12:37:16 -0400

    Add metadata fields to SessionSerializer

--- api/skystats/v1/shots/serializers.py
+++ api/skystats/v1/shots/serializers.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 
@@ -18,6 +20,8 @@ class ShotSerializer(serializers.ModelSerializer):
 
 class SessionSerializer(serializers.ModelSerializer):
     shots = ShotSerializer(many=True)
+    clubs_used = serializers.SerializerMethodField()
+    shots_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Session
@@ -34,3 +38,12 @@ class SessionSerializer(serializers.ModelSerializer):
         for shot in shots:
             Shot.objects.create(session=session, **shot)
         return session
+
+    def get_clubs_used(self, session: Session) -> List[str]:
+        clubs = set()
+        for shot in session.shots.all():
+            clubs.add(shot.club)
+        return list(clubs)
+
+    def get_shots_count(self, session: Session) -> int:
+        return session.shots.count()
