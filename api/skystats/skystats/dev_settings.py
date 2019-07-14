d122936303bf695a29ef194e7ccb7f8689634419
dknight <chaknight@deloitte.com>
2019-07-14 10:29:21 -0400

    Add cords headers for api

--- api/skystats/skystats/dev_settings.py
+++ api/skystats/skystats/dev_settings.py
@@ -1,2 +1,4 @@
 DEBUG = True
 SECRET_KEY = "secret key for dev"
+
+CORS_ORIGIN_WHITELIST = ["http://localhost:4200"]
--- api/skystats/skystats/prod_settings.py
+++ api/skystats/skystats/prod_settings.py
@@ -4,3 +4,5 @@ try:
     SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 except KeyError:
     raise ValueError("DJANGO_SECRET_KEY environment variable must be set")
+
+CORS_ORIGIN_WHITELIST = ["https://skytrakstats.com"]
