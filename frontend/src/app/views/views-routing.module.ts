9cc957fd32a9d7e1acbc0a453d8c11b5cee7820b
dknight <chaknight@deloitte.com>
2019-07-14 10:31:07 -0400

    Add sessions component and service

--- frontend/src/app/views/views-routing.module.ts
+++ frontend/src/app/views/views-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { SessionsComponent } from './sessions/sessions.component';
 
 const routes: Routes = [
-
+    { path: '', component: SessionsComponent },
 ];
 
 @NgModule({
--- frontend/src/app/views/views.module.ts
+++ frontend/src/app/views/views.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { ViewsRoutingModule } from './views-routing.module';
+import { SessionsComponent } from './sessions/sessions.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SessionsComponent],
   imports: [
     CommonModule,
-    ViewsRoutingModule
+    ViewsRoutingModule,
+    HttpClientModule
   ]
 })
 export class ViewsModule { }
