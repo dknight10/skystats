81a8def8948ee2f53366b6d01ae9aada7209e316
dknight <dknight1010@gmail.com>
2019-08-10 19:17:21 -0400

    Add datasource column class to handle display names

--- api/skystats/v1/export/data_source.py
+++ api/skystats/v1/export/data_source.py
@@ -1,17 +1,34 @@
-from typing import Any, Generator, Iterable, Sequence
+from typing import Any, Generator, Iterable, Sequence, Tuple, Union
+
+
+class DataSourceColumn:
+    def __init__(self, column):
+        if isinstance(column, tuple):
+            self.name = column[0]
+            self.display = column[1]
+        elif isinstance(column, str):
+            self.name = column
+            self.display = column
+        else:
+            raise ValueError("Column values should be tuple or str")
 
 
 class DataSource:
-    def __init__(self, name: str, columns: Sequence[str], data: Iterable[Any]):
+    def __init__(
+        self,
+        name: str,
+        columns: Sequence[Union[str, Tuple[str, str]]],
+        data: Iterable[Any],
+    ):
         self.name = name
-        self.columns = columns
+        self.columns = [DataSourceColumn(c) for c in columns]
         self._data = data
 
     def rows(self) -> Generator[Any, None, None]:
         return (row for row in self._data)
 
     def data(self) -> Generator[Sequence[str], None, None]:
-        yield self.columns
+        yield [c.display for c in self.columns]
 
         for row in self.rows():
-            yield [row[c] for c in self.columns]
+            yield [getattr(row, c.name) for c in self.columns]
