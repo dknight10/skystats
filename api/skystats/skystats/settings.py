f1b307461164545197da25588af3a91877918930
dknight <dknight1010@gmail.com>
2019-08-11 10:02:25 -0400

    Remove timezone configuration

--- api/skystats/skystats/settings.py
+++ api/skystats/skystats/settings.py
@@ -118,13 +118,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
-
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 AUTH0_URL = "https://dk-test.auth0.com/"
 
